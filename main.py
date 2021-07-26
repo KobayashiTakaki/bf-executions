872210acefba3d9a62170e2d7387bb8952c644d4
kobayashitakaki <o.share.thief@gmail.com>
2021-07-27 01:56:49 +0900

    feat: multiprocessing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import time
 import urllib
+from multiprocessing import Process, Queue
 from dotenv import load_dotenv
 import requests
 from influxdb_client import InfluxDBClient
@@ -11,34 +13,30 @@ org = 'takakisan.com'
 bucket = 'bf'
 load_dotenv(verbose=True)
 
-def fetch_executions():
+def fetch_executions(q):
     payload = {
         'product_code': product_code,
-        'count': 100
+        'count': 1000
     }
-    r = requests.get(
-        urllib.parse.urljoin(endpoint_url, '/v1/getexecutions'),
-        params=payload
-    )
-    executions = r.json()
-    client = get_client()
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    for e in executions:
-        print(e)
-        record = {
-            'measurement': 'executions',
-            'time': e['exec_date'],
-            'tags': {
-                'product': product_code,
-                'side': e['side']
-            },
-            'fields': {
-                'size': e['size'],
-                'price': e['price']
-            }
-        }
-        write_api.write(bucket, org, record=record)
-    client.close()
+    last_id = 0
+    while True:
+        if last_id != 0:
+            payload['after'] = last_id
+        try:
+            r = requests.get(
+                urllib.parse.urljoin(endpoint_url, '/v1/getexecutions'),
+                params=payload
+            )
+        except requests.exceptions.RequestException as e:
+            print('Error in requests: ', e)
+            time.sleep(6)
+            continue
+        executions = r.json()
+        q.put(executions)
+        ids = [e['id'] for e in executions]
+        if len(ids) > 0:
+            last_id = max(ids)
+        time.sleep(6)
 
 def get_client():
     return InfluxDBClient(
@@ -47,4 +45,26 @@ def get_client():
     )
 
 if __name__ == '__main__':
-    fetch_executions()
+    q = Queue()
+    p = Process(target=fetch_executions, args=(q,))
+    client = get_client()
+    p.start()
+    with client.write_api(write_options=SYNCHRONOUS) as write_api:
+        while True:
+            executions = q.get()
+            for e in executions:
+                print(e)
+                record = {
+                    'measurement': 'executions',
+                    'time': e['exec_date'],
+                    'tags': {
+                        'product': product_code,
+                        'side': e['side']
+                    },
+                    'fields': {
+                        'size': e['size'],
+                        'price': e['price']
+                    }
+                }
+                write_api.write(bucket, org, record=record)
+            time.sleep(1)
